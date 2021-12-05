54bbd2a89e65c968924a49c3f68264ff6f72aaf7
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2021-12-05 12:52:59 -0300

    adicionando arquivos

---
+++ src/main/java/br/com/api/ApiGestaoProdutosApplication.java
@@ -0,0 +1,13 @@
+package br.com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestaoProdutosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGestaoProdutosApplication.class, args);
+    }
+
+}
